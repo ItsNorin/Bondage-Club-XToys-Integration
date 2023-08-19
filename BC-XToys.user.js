4d682af1cc27aeb254ccf874cdd9ea3909d3b155
ItsNorin <itsnorin@gmail.com>
2023-08-19 00:50:33 -0700

    Auto connecting to websockets

--- BC-XToys.user.js
+++ BC-XToys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bondage Club XToys Integration
 // @namespace    BC-XToys
-// @version      0.5.8a
+// @version      0.5.8b
 // @description  Sends in game actions and toy activity to XToys.
 // @author       ItsNorin
 // @match        https://bondageprojects.elementfx.com/*
@@ -14,7 +14,7 @@
 // @grant        none
 // ==/UserScript==
 
-const BC_XToys_Version = "0.5.8a";
+const BC_XToys_Version = "0.5.8b";
 const BC_XToys_FullName = "Bondage Club XToys Integration";
 
 // Chat message contents to always ignore
@@ -26,6 +26,13 @@ const BC_XToysFirstJoinMsg = BC_XToys_FullName + ' v' + BC_XToys_Version + ' loa
 
 var bcModSdk = function () { "use strict"; const e = "1.1.0"; function o(e) { alert("Mod ERROR:\n" + e); const o = new Error(e); throw console.error(o), o } const t = new TextEncoder; function n(e) { return !!e && "object" == typeof e && !Array.isArray(e) } function r(e) { const o = new Set; return e.filter((e => !o.has(e) && o.add(e))) } const i = new Map, a = new Set; function d(e) { a.has(e) || (a.add(e), console.warn(e)) } function s(e) { const o = [], t = new Map, n = new Set; for (const r of p.values()) { const i = r.patching.get(e.name); if (i) { o.push(...i.hooks); for (const [o, a] of i.patches.entries()) t.has(o) && t.get(o) !== a && d(`ModSDK: Mod '${r.name}' is patching function ${e.name} with same pattern that is already applied by different mod, but with different pattern:\nPattern:\n${o}\nPatch1:\n${t.get(o) || ""}\nPatch2:\n${a}`), t.set(o, a), n.add(r.name) } } o.sort(((e, o) => o.priority - e.priority)); const r = function (e, o) { if (0 === o.size) return e; let t = e.toString().replaceAll("\r\n", "\n"); for (const [n, r] of o.entries()) t.includes(n) || d(`ModSDK: Patching ${e.name}: Patch ${n} not applied`), t = t.replaceAll(n, r); return (0, eval)(`(${t})`) }(e.original, t); let i = function (o) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookChainExit) || void 0 === i ? void 0 : i.call(t, e.name, n), d = r.apply(this, o); return null == a || a(), d }; for (let t = o.length - 1; t >= 0; t--) { const n = o[t], r = i; i = function (o) { var t, i; const a = null === (i = (t = m.errorReporterHooks).hookEnter) || void 0 === i ? void 0 : i.call(t, e.name, n.mod), d = n.hook.apply(this, [o, e => { if (1 !== arguments.length || !Array.isArray(o)) throw new Error(`Mod ${n.mod} failed to call next hook: Expected args to be array, got ${typeof e}`); return r.call(this, e) }]); return null == a || a(), d } } return { hooks: o, patches: t, patchesSources: n, enter: i, final: r } } function c(e, o = !1) { let r = i.get(e); if (r) o && (r.precomputed = s(r)); else { let o = window; const a = e.split("."); for (let t = 0; t < a.length - 1; t++)if (o = o[a[t]], !n(o)) throw new Error(`ModSDK: Function ${e} to be patched not found; ${a.slice(0, t + 1).join(".")} is not object`); const d = o[a[a.length - 1]]; if ("function" != typeof d) throw new Error(`ModSDK: Function ${e} to be patched not found`); const c = function (e) { let o = -1; for (const n of t.encode(e)) { let e = 255 & (o ^ n); for (let o = 0; o < 8; o++)e = 1 & e ? -306674912 ^ e >>> 1 : e >>> 1; o = o >>> 8 ^ e } return ((-1 ^ o) >>> 0).toString(16).padStart(8, "0").toUpperCase() }(d.toString().replaceAll("\r\n", "\n")), l = { name: e, original: d, originalHash: c }; r = Object.assign(Object.assign({}, l), { precomputed: s(l), router: () => { }, context: o, contextProperty: a[a.length - 1] }), r.router = function (e) { return function (...o) { return e.precomputed.enter.apply(this, [o]) } }(r), i.set(e, r), o[r.contextProperty] = r.router } return r } function l() { const e = new Set; for (const o of p.values()) for (const t of o.patching.keys()) e.add(t); for (const o of i.keys()) e.add(o); for (const o of e) c(o, !0) } function f() { const e = new Map; for (const [o, t] of i) e.set(o, { name: o, original: t.original, originalHash: t.originalHash, sdkEntrypoint: t.router, currentEntrypoint: t.context[t.contextProperty], hookedByMods: r(t.precomputed.hooks.map((e => e.mod))), patchedByMods: Array.from(t.precomputed.patchesSources) }); return e } const p = new Map; function u(e) { p.get(e.name) !== e && o(`Failed to unload mod '${e.name}': Not registered`), p.delete(e.name), e.loaded = !1, l() } function g(e, t, r) { "string" == typeof e && "string" == typeof t && (alert(`Mod SDK warning: Mod '${e}' is registering in a deprecated way.\nIt will work for now, but please inform author to update.`), e = { name: e, fullName: e, version: t }, t = { allowReplace: !0 === r }), e && "object" == typeof e || o("Failed to register mod: Expected info object, got " + typeof e), "string" == typeof e.name && e.name || o("Failed to register mod: Expected name to be non-empty string, got " + typeof e.name); let i = `'${e.name}'`; "string" == typeof e.fullName && e.fullName || o(`Failed to register mod ${i}: Expected fullName to be non-empty string, got ${typeof e.fullName}`), i = `'${e.fullName} (${e.name})'`, "string" != typeof e.version && o(`Failed to register mod ${i}: Expected version to be string, got ${typeof e.version}`), e.repository || (e.repository = void 0), void 0 !== e.repository && "string" != typeof e.repository && o(`Failed to register mod ${i}: Expected repository to be undefined or string, got ${typeof e.version}`), null == t && (t = {}), t && "object" == typeof t || o(`Failed to register mod ${i}: Expected options to be undefined or object, got ${typeof t}`); const a = !0 === t.allowReplace, d = p.get(e.name); d && (d.allowReplace && a || o(`Refusing to load mod ${i}: it is already loaded and doesn't allow being replaced.\nWas the mod loaded multiple times?`), u(d)); const s = e => { "string" == typeof e && e || o(`Mod ${i} failed to patch a function: Expected function name string, got ${typeof e}`); let t = g.patching.get(e); return t || (t = { hooks: [], patches: new Map }, g.patching.set(e, t)), t }, f = { unload: () => u(g), hookFunction: (e, t, n) => { g.loaded || o(`Mod ${i} attempted to call SDK function after being unloaded`); const r = s(e); "number" != typeof t && o(`Mod ${i} failed to hook function '${e}': Expected priority number, got ${typeof t}`), "function" != typeof n && o(`Mod ${i} failed to hook function '${e}': Expected hook function, got ${typeof n}`); const a = { mod: g.name, priority: t, hook: n }; return r.hooks.push(a), l(), () => { const e = r.hooks.indexOf(a); e >= 0 && (r.hooks.splice(e, 1), l()) } }, patchFunction: (e, t) => { g.loaded || o(`Mod ${i} attempted to call SDK function after being unloaded`); const r = s(e); n(t) || o(`Mod ${i} failed to patch function '${e}': Expected patches object, got ${typeof t}`); for (const [n, a] of Object.entries(t)) "string" == typeof a ? r.patches.set(n, a) : null === a ? r.patches.delete(n) : o(`Mod ${i} failed to patch function '${e}': Invalid format of patch '${n}'`); l() }, removePatches: e => { g.loaded || o(`Mod ${i} attempted to call SDK function after being unloaded`); s(e).patches.clear(), l() }, callOriginal: (e, t, n) => (g.loaded || o(`Mod ${i} attempted to call SDK function after being unloaded`), "string" == typeof e && e || o(`Mod ${i} failed to call a function: Expected function name string, got ${typeof e}`), Array.isArray(t) || o(`Mod ${i} failed to call a function: Expected args array, got ${typeof t}`), function (e, o, t = window) { return c(e).original.apply(t, o) }(e, t, n)), getOriginalHash: e => ("string" == typeof e && e || o(`Mod ${i} failed to get hash: Expected function name string, got ${typeof e}`), c(e).originalHash) }, g = { name: e.name, fullName: e.fullName, version: e.version, repository: e.repository, allowReplace: a, api: f, loaded: !0, patching: new Map }; return p.set(e.name, g), Object.freeze(f) } function h() { const e = []; for (const o of p.values()) e.push({ name: o.name, fullName: o.fullName, version: o.version, repository: o.repository }); return e } let m; const y = function () { if (void 0 === window.bcModSdk) return window.bcModSdk = function () { const o = { version: e, apiVersion: 1, registerMod: g, getModsInfo: h, getPatchingInfo: f, errorReporterHooks: Object.seal({ hookEnter: null, hookChainExit: null }) }; return m = o, Object.freeze(o) }(); if (n(window.bcModSdk) || o("Failed to init Mod SDK: Name already in use"), 1 !== window.bcModSdk.apiVersion && o(`Failed to init Mod SDK: Different version already loaded ('1.1.0' vs '${window.bcModSdk.version}')`), window.bcModSdk.version !== e && (alert(`Mod SDK warning: Loading different but compatible versions ('1.1.0' vs '${window.bcModSdk.version}')\nOne of mods you are using is using an old version of SDK. It will work for now but please inform author to update`), window.bcModSdk.version.startsWith("1.0.") && void 0 === window.bcModSdk._shim10register)) { const e = window.bcModSdk, o = Object.freeze(Object.assign(Object.assign({}, e), { registerMod: (o, t, n) => o && "object" == typeof o && "string" == typeof o.name && "string" == typeof o.version ? e.registerMod(o.name, o.version, "object" == typeof t && !!t && !0 === t.allowReplace) : e.registerMod(o, t, n), _shim10register: !0 })); window.bcModSdk = o } return window.bcModSdk }(); return "undefined" != typeof exports && (Object.defineProperty(exports, "__esModule", { value: !0 }), exports.default = y), y }();
 
+const modApi = bcModSdk.registerMod({
+    name: "Bondage Club XToys Integration",
+    fullName: "BC-XToys",
+    version: BC_XToys_Version,
+    repository: "https://github.com/ItsNorin/Bondage-Club-XToys-Integration",
+});
+
 var BC_XToys_defaultPunishShockLevel = 1;
 
 // Websocket manager to handle connections and sending messages
@@ -35,6 +42,37 @@ const BC_XToys_Websockets = {
     // if false, will not send anything
     sendMessages: true,
 
+    // whether to attempt to reconnect when a socket comes offline
+    autoReconnectMap: new Map(),
+
+    saveSockets() {
+        var urls = JSON.stringify(Array.from(this.sockets.keys()));
+        console.log("BC-XToys: Saving urls to local storage: " + urls);
+        localStorage.setItem("BC-XToys Websockets", urls);
+    },
+
+    // whether to try to connect to locally saved websockets on startup
+    setAutoConnectState(e) {
+        if (e === true || e === false) {
+            localStorage.setItem("BC-XToys AutoConnect", JSON.stringify(e));
+        }
+    },
+    getAutoConnectState() {
+        var s = JSON.parse(localStorage.getItem("BC-XToys AutoConnect"));
+        return (s === true) ? true : false;
+    },
+
+    // whether to attempt to reconnect when a socket comes offline
+    setAutoReconnectState(e) {
+        if (e === true || e === false) {
+            localStorage.setItem("BC-XToys AutoReconnect", JSON.stringify(e));
+        }
+    },
+    getAutoReconnectState() {
+        var s = JSON.parse(localStorage.getItem("BC-XToys AutoReconnect"));
+        return (s === true) ? true : false;
+    },
+
     // creates a websocket connection to given URL
     connect(url) {
         if (url == null) { return; }
@@ -42,16 +80,25 @@ const BC_XToys_Websockets = {
         if (this.hasConnection(url)) {
             var m = 'Already connected to ' + url;
             ChatRoomSendLocal(m, 60000);
-            console.log(m);
+            console.log("BC-XToys: " + m);
             return;
         }
 
         var newSocket = new WebSocket(url);
+        this.sockets.set(url, newSocket);
 
         newSocket.onopen = function (e) {
             var m = 'Connected to ' + newSocket.url;
             ChatRoomSendLocal(m, 60000);
-            console.log(m);
+            console.log("BC-XToys: " + m);
+
+            if (BC_XToys_Websockets.getAutoConnectState() == true) {
+                BC_XToys_Websockets.saveSockets();
+            }
+
+            if (BC_XToys_Websockets.getAutoReconnectState() == true) {
+                BC_XToys_Websockets.autoReconnectMap.set(url, 3);
+            }
         };
         newSocket.onmessage = e => {
             console.log(e.data);
@@ -59,11 +106,34 @@ const BC_XToys_Websockets = {
         newSocket.onclose = function (event) {
             var m = 'Disconnected from ' + url;
             ChatRoomSendLocal(m, 60000);
-            console.log(m);
-            BC_XToys_Websockets.close(url);
-        };
+            console.log("BC-XToys: " + m);
+            close(url);
 
-        this.sockets.set(url, newSocket);
+            // attempt reconnections if desired
+            if (BC_XToys_Websockets.getAutoReconnectState() == true) {
+                var attempts = BC_XToys_Websockets.autoReconnectMap.get(url);
+                if (attempts > 0) {
+                    setTimeout(function () {
+                        BC_XToys_Websockets.autoReconnectMap.set(url, attempts - 1);
+                        BC_XToys_Websockets.connect(url);
+                    }, 100);
+                } else {
+                    BC_XToys_Websockets.autoReconnectMap.delete(url);
+                }
+            }
+        };
+    },
+
+    connectToSavedSocketsIfAllowed() {
+        if (this.getAutoConnectState() != true) { return; }
+        var urls = JSON.parse(localStorage.getItem("BC-XToys Websockets"));
+        console.log("BC-XToys: Connecting to urls from local storage: ");
+
+        if (Array.isArray(urls)) {
+            for (var u in urls) {
+                this.connect(urls[u]);
+            }
+        }
     },
 
     // sends to all connected sockets
@@ -75,7 +145,7 @@ const BC_XToys_Websockets = {
             logMsg += s.url + ', ';
             s.send(text);
         }
-        console.log(logMsg);
+        console.log("BC-XToys: " + logMsg);
     },
 
     // sends formatted arguements to all connected websockets
@@ -84,7 +154,7 @@ const BC_XToys_Websockets = {
     // if arg data is null, will send "none"
     sendFormattedArgs(actionName, args = null) {
         if (!this.hasAnyConnection()) {
-            console.log('Failed to send message, not connected.');
+            console.log('BC-XToys: Failed to send message, not connected.');
             return;
         }
 
@@ -131,12 +201,16 @@ const BC_XToys_Websockets = {
 
     // closes and removes connection to url
     close(url) {
+        this.autoReconnectMap.delete(url);
+
         var s = this.sockets.get(url);
         if (s == null) { return; }
         if (s.readyState <= 1) {
             s.close(1000);
         }
         this.sockets.delete(url);
+
+        this.saveSockets();
     },
 
     // closes all connections
@@ -154,6 +228,18 @@ const BC_XToys_Websockets = {
         return c;
     },
 };
+
+// connecting to websockets every time server gains connection
+modApi.hookFunction(
+    'ServerSetConnected',
+    2,
+    (args, next) => {
+        next(args);
+        if (args[0] == true) {
+            BC_XToys_Websockets.connectToSavedSocketsIfAllowed();
+        }
+    }
+);
 
 // Ongoing slot state handler, use this to avoid sending duplicate messages for restraint and toy related events
 const Item_State_Handler = (function () {
@@ -267,7 +353,7 @@ const Item_State_Handler = (function () {
                 this.updateItemProperties('Inflation', 'inflationEvent', slot, this.getItemName(slot), 0);
             }
             this.clearSlot(slot);
-        }
+        },
     };
 })();
 
@@ -275,13 +361,6 @@ const Item_State_Handler = (function () {
 (async function () {
     await waitFor(() => ServerIsConnected && ServerSocket);
     await waitFor(() => !!Commands);
-
-    const modApi = bcModSdk.registerMod({
-        name: "Bondage Club XToys Integration",
-        fullName: "BC-XToys",
-        version: BC_XToys_Version,
-        repository: "https://github.com/ItsNorin/Bondage-Club-XToys-Integration",
-    });
 
     console.log("Starting " + BC_XToys_FullName + " version " + BC_XToys_Version + ".");
 
@@ -486,6 +565,16 @@ const Item_State_Handler = (function () {
                 + '<b>/bcxtoys resume</b>\n'
                 + 'Resumes sending messages to connected sockets if /bcxtoys pause was used. Can use "r" instead\n'
 
+                + '<b>/bcxtoys auto_connect</b>\n'
+                + 'On startup, will attempt to connect to any websocket connected to after being turned on. Currently <b>'
+                + (BC_XToys_Websockets.getAutoConnectState() == true ? 'enabled' : 'disabled')
+                + '</b>\n'
+
+                + '<b>/bcxtoys auto_reconnect</b>\n'
+                + 'Will attempt to reconnect 3 times any time a websocket disconnects unintentionally. Currently <b>'
+                + (BC_XToys_Websockets.getAutoReconnectState() == true ? 'enabled' : 'disabled')
+                + '</b>\n'
+
                 + '<b>/bcxtoys punish_shock_level [0-2]</b>\n'
                 + 'Sets intensity of shock to send for items that do not have explicit shock intensities, 0 being weakest, 2 strongest\n'
                 ;
@@ -564,6 +653,20 @@ const Item_State_Handler = (function () {
                         ChatRoomSendLocal('Resumed sending messages to websockets. Use <b>/bcxtoys pause</b> to pause');
                         break;
 
+                    case 'auto_connect':
+                        BC_XToys_Websockets.setAutoConnectState(!BC_XToys_Websockets.getAutoConnectState())
+                        ChatRoomSendLocal('Auto connect is now <b>'
+                            + (BC_XToys_Websockets.getAutoConnectState() == true ? 'enabled' : 'disabled')
+                            + '</b>')
+                        break;
+
+                    case 'auto_reconnect':
+                        BC_XToys_Websockets.setAutoReconnectState(!BC_XToys_Websockets.getAutoReconnectState());
+                        ChatRoomSendLocal('Auto reconnect is now <b>'
+                            + (BC_XToys_Websockets.getAutoReconnectState() == true ? 'enabled' : 'disabled')
+                            + '</b>')
+                        break;
+
                     case 'punish_shock_level':
                         if (args.length < 2 || args[1] < 0 || args[1] > 2) {
                             ChatRoomSendLocal('Enter a valid level, [0-2]', 30000);
@@ -601,7 +704,7 @@ const Item_State_Handler = (function () {
 
         sendFirstChatroomMsg(data);
 
-        console.log(data);
+        //console.log(data);
 
         handlePortalLink(data);
         handleActivities(data);
@@ -738,33 +841,6 @@ const Item_State_Handler = (function () {
             }
         }
     );
-
-    /*
-    modApi.hookFunction(
-        'ChatRoomPublishCustomAction',
-        3,
-        (args, next) => {
-            next(args);
-
-            console.log("ChatRoomPublishCustomAction");
-            console.log(args);
-        }
-    );
-
-    modApi.hookFunction(
-        'VibratorModeRegister',
-        3,
-        (args, next) => {
-            next(args);
-
-            console.log("VibratorModeRegister");
-            console.log(args);
-        }
-    );
-    */
-
-
-
 })();
 
 //
