5f0a16c76b2a504f238b0eacf77bdb400977785a
ItsNorin <itsnorin@gmail.com>
2023-09-13 16:03:09 -0700

    Repo consolidation

--- BC-XToysLoader.user.js
+++ BC-XToysLoader.user.js
@@ -3,14 +3,14 @@
 // @namespace    BCXToys
 // @description  Sends in game actions and toy activity to XToys.
 // @author       ItsNorin
-// @version      1.0
+// @version      1.1
 // @match        https://bondageprojects.elementfx.com/*
 // @match        https://www.bondageprojects.elementfx.com/*
 // @match        https://bondage-europe.com/*
 // @match        https://www.bondage-europe.com/*
 // @match        http://localhost:*/*
 // @homepage     https://github.com/ItsNorin/Bondage-Club-XToys-Integration
-// @downloadURL  https://itsnorin.github.io/BC-XToys/BC-XToysLoader.user.js
+// @downloadURL  https://itsnorin.github.io/Bondage-Club-XToys-Integration/BC-XToysLoader.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -21,7 +21,7 @@ setTimeout(
         const n = document.createElement("script");
         n.language = "JavaScript";
         n.crossorigin = "anonymous";
-        n.src = "https://itsnorin.github.io/BC-XToys/BC-XToys.user.js";
+        n.src = "https://itsnorin.github.io/Bondage-Club-XToys-Integration/BC-XToys.user.js";
         document.head.appendChild(n);
     },
     2000,
